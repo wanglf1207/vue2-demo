b8601036be49dfa991f04f5fbf6af57407f92d0b
wanglf <wanglf@163.com>
2021-01-22 11:14:02 +0800

    qui pages

--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,34 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
+import index from '../pages/index.vue'
+import pageQuiButtion from '../pages/pageQuiButton'
+import pageQuiList from '../pages/pageQuiList'
+import pageQuiNav from '../pages/pageQuiNav'
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'index',
+      component: index
+    },
+    {
+      path: '/btn',
+      name: 'btn',
+      component: pageQuiButtion
+    },
+    {
+      path: '/list',
+      name: 'list',
+      component: pageQuiList
+    },
+    {
+      path: '/nav',
+      name: 'nav',
+      component: pageQuiNav
     }
   ]
 })
